60d0eb20b3e9b0222e63b75a1271385eefa09a8c
nsabbatini <Narcizo.Sabbatini@gmail.com>
2022-04-27 09:53:59 -0300

    fixes

--- 3-packages.sh
+++ 3-packages.sh
@@ -5,7 +5,7 @@ pacman -S efibootmgr apparmor bash-completion man-db man-pages dialog logrotate 
 pacman -S networkmanager bluez bluez-utils avahi inetutils dnsutils wireless_tools
 pacman -S openssh sshfs fuse3 rsync curl wget
 pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
-#pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
+#pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth
 pacman -S mesa intel-media-driver vulkan-intel xf86-input-synaptics
 pacman -S nftables flatpak cups cups-pdf haveged
 echo "finished package download"
--- basic.sh
+++ basic.sh
@@ -1,15 +1,8 @@
 #!bin/bash
 
 # ############################################
-# Swap file, location, time zone, hostname
+# Location, time zone, hostname
 # ############################################
-
-# Create swap file
-dd if=/dev/zero of=/swapfile bs=1M count=2048 status=progress
-chmod 600 /swapfile
-mkswap /swapfile
-echo '/swapfile none swap defaults 0 0' | tee -a /etc/fstab
-echo "swapfile created"
 
 # Set the time zone and enable hardware clock
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
@@ -46,9 +39,11 @@ pacman -S efibootmgr apparmor bash-completion man-db man-pages dialog logrotate 
 pacman -S networkmanager bluez bluez-utils avahi inetutils dnsutils wireless_tools
 pacman -S openssh sshfs fuse3 rsync curl wget
 pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
-#pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
+#pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth
 pacman -S mesa intel-media-driver vulkan-intel xf86-input-synaptics
 pacman -S nftables flatpak cups cups-pdf haveged
+sudo pacman -S gnome gnome-tweaks dconf-editor
+sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-roboto ttf-roboto-mono adobe-source-sans-fonts adobe-source-code-pro-fonts
 echo "finished package download"
 
 # Enable basic services
@@ -64,6 +59,7 @@ systemctl enable wpa_supplicant
 systemctl enable bluetooth
 systemctl enable systemd-boot-update
 systemctl enable systemd-timesyncd
+systemctl enable gdm
 echo "systemd services enabled"
 
 # ###########
