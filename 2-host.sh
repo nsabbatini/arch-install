d9e540e359d76faa11d5b15ca20ac931eb970d27
nsabbatini <Narcizo.Sabbatini@gmail.com>
2022-05-09 09:56:05 -0300

    fixes

--- 2-host.sh
+++ 2-host.sh
@@ -7,6 +7,5 @@ echo "host name configured"
 # Create /etc/hosts
 { echo "127.0.0.1 localhost";
   echo "::1       localhost";
-  echo "127.0.0.1 lince.localdomain lince";
 } > /etc/hosts
 echo "/etc/hosts created"
